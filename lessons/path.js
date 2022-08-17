7098b932a2c7464067e0a29a9ac7b1155ff6f74a
Andryii <andryii.protasenko@gmial.com>
2022-08-17 21:49:31 +0300

    url

--- lessons/path.js
+++ lessons/path.js
@@ -1,10 +1,16 @@
-const path = require("path");
+// const path = require("path");
 
-const fullpath = path.resolve("first", "second.js");
-console.log("Парсинг пути", path.parse(fullpath));
-console.log("разделитель в ОС", path.sep);
-console.log("Проверка на абсолютній путь", path.isAbsolute("first/second"));
-console.log("Название файла", path.basename(fullpath));
-console.log('Расширение файла', path.extname(fullpath));
+// const fullpath = path.resolve("first", "second.js");
+// console.log("Парсинг пути", path.parse(fullpath));
+// console.log("разделитель в ОС", path.sep);
+// console.log("Проверка на абсолютній путь", path.isAbsolute("first/second"));
+// console.log("Название файла", path.basename(fullpath));
+// console.log('Расширение файла', path.extname(fullpath));
 
+//----------------------------------
 
+const siteURL = "http://localhost:8080/users?id=5123";
+
+const url = new URL(siteURL);
+
+console.log(url);
