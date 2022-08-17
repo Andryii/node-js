eed2b3036bc4162764dbd385932c7ddc4ef2da18
Andryii <andryii.protasenko@gmial.com>
2022-08-17 22:08:57 +0300

    hell callback

--- lessons/file-system.js
+++ lessons/file-system.js
@@ -1,11 +1,34 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-fs.mkdir(path.resolve(__dirname,'dir'), (err)=>
-{
+// fs.mkdir(path.resolve(__dirname,'dir'), (err)=>
+// {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log('Папка созадана');
+// })
+
+// fs.rmdir(path.resolve(__dirname, "dir"), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Папка удалена");
+// });
+
+fs.writeFile(path.resolve(__dirname, "text.txt"), "5 qwerty 123456", (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Файл записан');
+
+  fs.appendFile(path.resolve(__dirname, "text.txt"), "zxcb 7 8 9 10", (err) => {
     if (err) {
-        console.log(err);
-        return;
+      throw err;
     }
-    console.log('Папка созадана');
-})
+    console.log('Файл записан');
+  });
+
+});
+
