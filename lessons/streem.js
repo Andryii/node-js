e9d3d7185d2f4a1dc4d5725e43e7f90d8ba93298
Andryii <andryii.protasenko@gmial.com>
2022-08-26 21:12:28 +0300

    Some chenges

--- lessons/streem.js
+++ lessons/streem.js
@@ -9,28 +9,36 @@ const path = require("path");
 //     console.log(data);
 // })
 
-const stream = fs.createReadStream(path.resolve(__dirname, "test.txt"));
+// const stream = fs.createReadStream(path.resolve(__dirname, "test.txt"));
 
-stream.on("data", (chunk) => {
-  console.log(chunk);
-});
+// stream.on("data", (chunk) => {
+//   console.log(chunk);
+// });
 
-stream.on("end", () => {
-  console.log("Закончили читать");
-});
+// stream.on("end", () => {
+//   console.log("Закончили читать");
+// });
 
-stream.on("open", () => console.log("Начали читать"));
-stream.on("error", (e) => {
-  console.log(e.message);
-});
+// stream.on("open", () => console.log("Начали читать"));
+// stream.on("error", (e) => {
+//   console.log(e.message);
+// });
 
-const writableStream = fs.createWriteStream(
-  path.resolve(__dirname, "test2.txt")
-);
+// const writableStream = fs.createWriteStream(
+//   path.resolve(__dirname, "test2.txt")
+// );
 
-for (let i = 0; i < 20; i++) {
-  writableStream.write(i + "\n");
-}
+// for (let i = 0; i < 20; i++) {
+//   writableStream.write(i + "\n");
+// }
 
-writableStream.end();
+// writableStream.end();
 
+// const http = require("http");
+
+// http.createServer((req, res) => {
+//   const stream = fs.createReadStream(path.resolve(__dirname, "text.txt"));
+  
+//     stream.pipe(res)
+
+// });
